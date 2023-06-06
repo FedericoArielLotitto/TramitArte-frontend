6e573f30c86c341d42b17194e0690c607996ef4d
Leandro Matías Gómez <lmgomez@DH.digitalhouse.com>
2023-06-06 02:50:46 -0300

    agrego registro y login

---
+++ src/app/registro/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import BasicCardRegistro from "@/components/moleculas/card/CardRegistro";
+import "./registroLogin.css"
+
+function Registro() {
+    return  (
+      <div class="card-container">
+      <BasicCardRegistro/>
+      </div>
+    )
+}
+
+export default Registro;
--- src/components/atomos/button/submit/default/ButtonSubmit.js
+++ src/components/atomos/button/submit/default/ButtonSubmit.js
@@ -4,7 +4,7 @@ const { Button, ThemeProvider } = require("@mui/material");
 
 const ButtonSubmit = ({ texto }) => (
   <ThemeProvider theme={theme}>
-    <Button sx={{ borderRadius:'45px'}} variant="contained" color="primary">{texto}</Button>
+    <Button sx={{ borderRadius:'45px',minWidth:"120px"}} variant="contained" color="primary">{texto}</Button>
   </ThemeProvider>
 );
 
