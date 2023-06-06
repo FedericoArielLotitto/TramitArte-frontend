5e46fa1478c3e63d763d7665e7cf055e1e275bf9
MatePasto <mateo.pastorini@gmail.com>
2023-06-06 19:45:19 -0300

    Modification of translation request, mobile version

--- src/app/request-translation/page.js
+++ src/app/request-translation/page.js
@@ -2,80 +2,107 @@
 
 import "./page.css";
 import { useEffect, useState } from "react";
-import {
-  AppBar,
-  BottomNavigation,
-  BottomNavigationAction,
-  Box,
-  Button,
-  Card,
-  CardContent,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import {
-  AccountBoxOutlined,
-  HelpOutlineOutlined,
-  HomeOutlined,
-  MenuOutlined,
-  SearchOutlined,
-} from "@mui/icons-material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import Footer from "@/components/moleculas/footer/Footer";
 import CustomAppBar from "@/components/moleculas/appBar/CustomAppBar";
 
 export default function request() {
-  const [document, setDocument] = useState(null);
+  const [document, setDocument] = useState(false)
 
   const handleClickDocument = () => {
     console.log("Hola loco");
-    setDocument("Hola loco");
-  };
+    setDocument(true);
+  }
 
   const handleClick = () => {
     console.log("Hola loco");
-    setDocument("Hola loco");
-  };
-
-  useEffect(() => {
-    console.log(document);
-  });
+  }
 
   return (
     <div className="page-wrapper">
       <CustomAppBar />
-      {document == null ? (
-        <Box>
-          <Card className="background">
-            <CardContent>
-              <Typography>Cargar Documento</Typography>
+      {document == false ? (
+        <Box className="page-body background-color">
+            <Card className="card-body">
+              <Typography className="font-basics">Cargar Documento</Typography>
               <Button
-                className="primary-color"
-                sx={{ borderRadius: "5rem", padding: "1rem" }}
+                className="primary-color font-basics"
+                sx={{ borderRadius: "5rem", padding: "1rem", color: "white" }}
                 onClick={handleClickDocument}
               >
                 Cargar Documento
               </Button>
-            </CardContent>
-          </Card>
+            </Card>
         </Box>
       ) : (
-        <Box color="white">
-          <Typography>Buscar Traductor</Typography>
-          <Card>
-            <Typography>Nombre - Apellido</Typography>
-            <Typography>Costo por traduccion: $ 777.777</Typography>
+        <Box
+          className="page-body background-white"
+        >
+          <Typography className="font-secondary">Buscar Traductor</Typography>
+          
+          <Card className="card-body">
+            <Typography className="font-secondary">Nombre - Apellido</Typography>
+            <Typography className="font-secondary">Costo por traduccion: $ 777.777</Typography>
             <Button
-              className="primary-color"
+              className="primary-color font-basics"
               sx={{ borderRadius: "5rem", padding: "1rem" }}
               onClick={handleClick}
             >
               Solicitar
             </Button>
           </Card>
+
+          <Card className="card-body">
+            <Typography className="font-secondary">Nombre - Apellido</Typography>
+            <Typography className="font-secondary">Costo por traduccion: $ 777.777</Typography>
+            <Button
+              className="primary-color font-basics"
+              sx={{ borderRadius: "5rem", padding: "1rem" }}
+              onClick={handleClick}
+            >
+              Solicitar
+            </Button>
+          </Card>
+
+          <Card className="card-body">
+            <Typography className="font-secondary">Nombre - Apellido</Typography>
+            <Typography className="font-secondary">Costo por traduccion: $ 777.777</Typography>
+            <Button
+              className="primary-color font-basics"
+              sx={{ borderRadius: "5rem", padding: "1rem" }}
+              onClick={handleClick}
+            >
+              Solicitar
+            </Button>
+          </Card>
+
+          <Card className="card-body">
+            <Typography className="font-secondary">Nombre - Apellido</Typography>
+            <Typography className="font-secondary">Costo por traduccion: $ 777.777</Typography>
+            <Button
+              className="primary-color font-basics"
+              sx={{ borderRadius: "5rem", padding: "1rem" }}
+              onClick={handleClick}
+            >
+              Solicitar
+            </Button>
+          </Card>
+
+          <Card className="card-body">
+            <Typography className="font-secondary">Nombre - Apellido</Typography>
+            <Typography className="font-secondary">Costo por traduccion: $ 777.777</Typography>
+            <Button
+              className="primary-color font-basics"
+              sx={{ borderRadius: "5rem", padding: "1rem" }}
+              onClick={handleClick}
+            >
+              Solicitar
+            </Button>
+          </Card>
+
         </Box>
       )}
       <Footer />
     </div>
-  );
+  )
 }
