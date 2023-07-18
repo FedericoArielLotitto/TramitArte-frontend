9b4728d6d32bf84250fc5ed35f49748d0c709679
FedericoArielLotitto <federicoariellotitto@gmail.com>
2023-07-17 21:43:30 -0300

    Corrige padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,6 +51,7 @@ function Home() {
         align={"center"}
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}
+        px={{md: 28}}
         direction={{ base: "column", md: "row" }}
       >
         <Stack flex={1} spacing={{ base: 5, md: 10 }}>
